6a4d51942b921421dbd98c8ca5cc8b7109364790
naveen <naveenreddy.k07@gmail.com>
2021-10-29 09:50:27 +0530

    first commit

---
+++ EmployeWage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPartTime = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(isPartTime==empCheck)
+            {
+                Console.WriteLine("Employ is present");
+            }
+            else
+            {
+                Console.WriteLine("Employe is absent");
+            }
+            Console.ReadKey();
+        }
+    }
+}
